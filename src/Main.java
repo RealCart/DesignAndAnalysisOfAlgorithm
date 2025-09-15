f7dc2590bfb02242495a2aa97c40f100d64536b9
beka <beka-nishanbek@mail.ru>
2025-09-15 14:47:54 +0500

    add merge sort

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-//TIP Чтобы <b>запустить</b> код, нажмите <shortcut actionId="Run"/> или
-// нажмите на значок <icon src="AllIcons.Actions.Execute"/> на полях.
+
 public class Main {
     public static void main(String[] args) {
 
