1da25996ec6f5cd1b716021c97a86490bcec8691
beka <beka-nishanbek@mail.ru>
2025-09-22 01:30:18 +0500

    add metrics

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import features.MergeSort;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        final int[] array = {1, 11, 234, 312, 423, 578, 61, 765, 832, 945, 10};
 
+        System.out.println("Unsorted array:" + Arrays.toString(array));
+        final int[] sorted = MergeSort.sort(array);
+        System.out.println("Sorted array:" + Arrays.toString(sorted));
     }
 }
---
+++ src/features/Metrics.java
@@ -0,0 +1,17 @@
+package features;
+
+public final class Metrics {
+    public long comparisons;
+    public long moves;
+    public long allocations;
+    public int maxDepth;
+    private int depth;
+
+    public void enter() { depth++; if (depth > maxDepth) maxDepth = depth; }
+    public void exit()  { depth--; }
+    public void cmp()   { comparisons++; }
+    public void mv(long k) { moves += k; }
+    public void alloc(long bytes) { allocations += bytes; }
+    public void reset() { comparisons = moves = allocations = 0; maxDepth = depth = 0; }
+}
+
