ba2370e45b646c36fa2fac57cf1e97a37a6379b7
beka <beka-nishanbek@mail.ru>
2025-09-28 18:09:42 +0500

    add quick sort

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
 import features.MergeSort;
+import features.QuickSort;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        final int[] array = {1, 11, 234, 312, 423, 578, 61, 765, 832, 945, 10};
+//        final int[] array = {1, 11, 234, 312, 423, 578, 61, 765, 832, 945, 10};
+//
+//        System.out.println("Unsorted array:" + Arrays.toString(array));
+//        final int[] sorted = MergeSort.sort(array);
+//        System.out.println("Sorted array:" + Arrays.toString(sorted));
 
-        System.out.println("Unsorted array:" + Arrays.toString(array));
-        final int[] sorted = MergeSort.sort(array);
-        System.out.println("Sorted array:" + Arrays.toString(sorted));
+        final int[] list = {1, 11, 234, 312, 423, 578, 61, 765, 832, 945, 10};
+        System.out.println(Arrays.toString(QuickSort.sort(list)));
     }
 }
